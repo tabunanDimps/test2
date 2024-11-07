55e82fa2cbe64235861f2ef3a47e69acb20f7840
tabunanDimps <146501392+tabunanDimps@users.noreply.github.com>
2024-11-06 22:38:57 -0800

    Add files via upload

---
+++ phptabunan/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>My First Ubutu Server PHP Deployment</title>
+</head>
+<body>
+<p> My First Ubuntu Server PHP Deployment<p>
+</body>
+</html>
